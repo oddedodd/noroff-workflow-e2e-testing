f3f4c8a7e05e63b8cc74d17e9282865ab314f98e
Odd Egil Hegge Selnes <post@odde.org>
2024-10-22 15:38:30 +0200

    added cypress cli testing

--- cypress/e2e/3-testing/no.wikipedia.cy.js
+++ cypress/e2e/3-testing/no.wikipedia.cy.js
@@ -1,6 +1,6 @@
 describe("no.wikipedia.org", () => {
   it("can search for Noroff", () => {
-    cy.visit("https://no.wikipedia.org");
+    cy.visit("https://www.wikipedia.org/");
     cy.get("input#searchInput").type("Noroff{enter}", { delay: 500 });
     cy.get("h1").contains("Noroff");
   });
